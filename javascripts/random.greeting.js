18262ead745ef12e0afcc2ea1fc484c0ffbabc40
Derek Damron <damrondm@gmail.com>
2016-03-28 21:48:34 -0700

    Finalize random greetings

--- javascripts/random.greeting.js
+++ javascripts/random.greeting.js
@@ -2,6 +2,6 @@ var random_greeting = new Array ();
 random_greeting[0] = "Greetings!";
 random_greeting[1] = "¡Bienvenida!";
 random_greeting[2] = "Willkommen!";
-var r = Math.floor(3*Math.random())
+var i = Math.floor(3*Math.random())
 
-document.write(random_greeting[r]);
+document.write(random_greeting[i]);
