c4c3a0c3ca7581c8bd316090db8657c9130d8bd1
Derek Damron <damrondm@gmail.com>
2016-04-13 22:13:33 -0700

    Remove _site

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-# Just when you thought you'd had enough.
-
-https://derek-damron.github.io/
--- _site/javascripts/random.greeting.js
+++
@@ -1,7 +0,0 @@
-var random_greeting = new Array ();
-	random_greeting[0] = "Greetings!";
-	random_greeting[1] = "¡Bienvenida!";
-	random_greeting[2] = "Willkommen!";
-var i = Math.floor(3*Math.random())
-
-document.write(random_greeting[i]);
